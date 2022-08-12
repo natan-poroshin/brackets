66c9248a8c7ad89a30e3c5fd4002d23158ac673f
natan-poroshin <natan.poroshin@gmail.com>
2022-08-13 01:00:39 +0300

    feat: finished implementation of brackets analyzer. This implementation is not completed.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+
+  /** 
+ *  @param {String} str
+ *  @param {Array} bracketsConfig
+ * bracketsConfig = [['(', ')'], ['|', '|']] example
+ */
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+    const openBrackets = bracketsConfig.map(pair => pair[0]);
+    const closeBrackets = bracketsConfig.map(pair => pair[1]);
+  
+    for(let i = 0; i < str.length; i++){
+      let char = str[i];
+  
+      if(openBrackets.includes(char)){
+        stack.push(char);
+      }
+      
+      if(closeBrackets.includes(char)){
+        let index = closeBrackets.indexOf(char);
+        topChar = stack.pop();
+
+        if(topChar !== openBrackets[index]){
+            return false;
+        }
+      }
+    }
+
+    return stack.length === 0;
 }
